25e09b516485c6aa2fc5f5849ab6f8f03de91837
Eren <erenbas3453@gmail.com>
2024-04-03 19:20:05 +0300

    Add Users, Customers And Rentals To The Program
    - Add entities for user, customer and rental
    - Add data access classes for each of these entities.
    - Add bussiness classes for each of these entites.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -23,5 +23,21 @@ namespace Business.Constants {
         public static string ColorDeleted = "Renk silindi";
         public static string ColorUpdated = "Renk güncellendi";
         public static string ColorsListed = "Renk listelendi";
+
+        public static string UserAdded = "Kullanıcı eklendi";
+        public static string UserDeleted = "Kullanıcı silindi";
+        public static string UserUpdated = "Kullanıcı güncellendi";
+        public static string UsersListed = "Kullanıcılar listelendi";
+        
+        public static string CustomerAdded = "Müşteri eklendi";
+        public static string CustomerDeleted ="Müşteri silindi";
+        public static string CustomerUpdated ="Müşteri güncellendi";
+        public static string CustomersListed = "Müşteriler listelendi";
+
+        public static string RentalAdded = "Kiralama eklendi";
+        public static string RentalDeleted = "Kiralama silindi";
+        public static string RentalUpdated = "Kiralama güncellendi";
+        public static string RentalsListed = "Kiralamalar listelendi";
+        public static string RentalNotReturned = "Kiralanan araç teslim edilmedi";
     }
 }
